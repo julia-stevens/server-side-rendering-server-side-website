e432fab4e160deb0cd54f926015c87d43b89164f
julia-stevens <juliastevens02@gmail.com>
2025-03-10 12:33:41 +0100

    #9 404 response + render

--- server.js
+++ server.js
@@ -85,3 +85,7 @@ app.set('port', process.env.PORT || 8000)
 app.listen(app.get('port'), function () {
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+app.use((request, response, next) => {
+  response.status(404).render("error.liquid")
+})
